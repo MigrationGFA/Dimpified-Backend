43e9085e82f8187643152a963826691f62ca510d
ipsychDev <joy@gfa-tech.com>
2024-05-24 23:39:44 +0100

    Added help center routes

--- src/routes/customerCare.js
+++ src/routes/customerCare.js
@@ -7,8 +7,16 @@ const {
   contactUsCompleted,
 } = require("../controllers/CustomerCareController/contactUs");
 
+const { userHelpCenter, getAllHelpRequest, helpRequestCompleted } = require("../controllers/CustomerCareController/helpCenter")
+
 router.post("/contact-us", userContactUs);
 router.get("/all-contact-us", allContactUs);
 router.put("/contact-us/:id/completed", contactUsCompleted);
+
+
+//Help center routes
+router.post('/help-center', userHelpCenter)
+router.get('/get-all-help-request', getAllHelpRequest)
+router.put("/help-center/:requestId/completed", helpRequestCompleted);
 
 module.exports = router;
