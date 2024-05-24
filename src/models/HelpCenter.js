1ac15751866c24d71c5c3cf0d246be069dd0da33
ipsychDev <joy@gfa-tech.com>
2024-05-24 23:38:51 +0100

    created help center table

---
+++ src/models/HelpCenter.js
@@ -0,0 +1,59 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/dbConnect");
+const User = require("./Users");
+
+const HelpCenter = sequelize.define('HelpCenter', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    UserId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        // references: {
+        //     model: 'User',
+        //     key: 'id'
+        // }
+    },
+    role: {
+        type: DataTypes.ENUM("Creator"),
+        allowNull: false
+
+    },
+    reason: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
+    message: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
+    creatorId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    ecosystemId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    status: {
+        type: DataTypes.ENUM("pending", "completed"),
+        allowNull: false,
+        defaultValue: "pending",
+    },
+},
+    {
+        tableName: "HelpCenter",
+        timestamps: true,
+    }
+)
+
+HelpCenter.belongsTo(User, { foreignKey: "userId", targetKey: "id" });
+module.exports = HelpCenter
