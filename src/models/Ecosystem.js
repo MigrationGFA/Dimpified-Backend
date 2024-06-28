089d4cba20e3f5a020cbb07dca0b2171ffde89aa
ipsychDev <joy@gfa-tech.com>
2024-06-28 03:56:09 +0100

    added eco certificate field

--- src/models/Ecosystem.js
+++ src/models/Ecosystem.js
@@ -14,6 +14,12 @@ const ecosystemSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  ecoCertificate: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "dimpifiedEcoCertificate",
+    },
+  ],
   targetAudienceSector: {
     type: String,
     required: true,
