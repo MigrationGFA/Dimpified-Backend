21073a38fc615c603172e9723792def82f358ec3
ipsychDev <joy@gfa-tech.com>
2024-07-01 05:03:46 +0100

    End users certificate model

---
+++ src/models/EndUserCertificate.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const UserCertificateSchema = new mongoose.Schema({
+    certificateNumber: {
+        type: String,
+        required: true,
+    },
+    userid: {
+        type: Number
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: 'dimpifiedCourse',
+        required: true,
+    },
+    recipientName: {
+        type: String,
+        required: true,
+    },
+    recipientEmail: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    signature: {
+        type: String,
+        required: true,
+    },
+    skills: [{
+        type: String,
+    }],
+    issuerName: {
+        type: String,
+        required: true,
+    },
+    issuerTitle: {
+        type: String,
+        required: true,
+    },
+    logoUrl: {
+        type: String,
+        required: true,
+    },
+    backgroundImageUrl: {
+        type: String,
+        required: true,
+    },
+})
+
+module.exports = mongoose.model("dimpifiedUserCertificate", UserCertificateSchema);
