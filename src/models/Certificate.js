89a64e07335eb7eccd49a6b4809935dd9bcaafeb
ipsychDev <joy@gfa-tech.com>
2024-07-01 05:03:25 +0100

    Ecosystem certifcate model

---
+++ src/models/Certificate.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const CertificateSchema = new mongoose.Schema({
+    certificateNumber: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'dimpifiedCourse',
+        required: true,
+    },
+
+    signature: {
+        type: String,
+        required: true,
+    },
+    skills: [{
+        type: String,
+    }],
+    issuerName: {
+        type: String,
+        required: true,
+    },
+    issuerTitle: {
+        type: String,
+        required: true,
+    },
+    logoUrl: {
+        type: String,
+        required: true,
+    },
+    backgroundImageUrl: {
+        type: String,
+        required: true,
+    },
+})
+
+module.exports = mongoose.model("dimpifiedEcoCertificate", CertificateSchema);
