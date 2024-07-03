a1c481fe20489e7460de9ce85eeebd9ba09a3201
Samuel-Makinde <makindesamuel1999@gmail.com>
2024-07-03 15:04:38 +0100

    made another change

--- src/routes/forms.js
+++ src/routes/forms.js
@@ -9,7 +9,7 @@ const {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/form/");
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
