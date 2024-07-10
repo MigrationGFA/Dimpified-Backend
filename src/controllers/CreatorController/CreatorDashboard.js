b7b6d5cd178fe0f0a9f9cdbe555b8732c1087fb2
Ajibade-gfa <Ajibade@gfa-tech.com>
2024-07-10 14:08:23 +0100

    Ecosystem User endpoints

--- src/controllers/CreatorController/CreatorDashboard.js
+++ src/controllers/CreatorController/CreatorDashboard.js
@@ -1,5 +1,7 @@
 const Ecosystem = require("../../models/Ecosystem");
+const EcosystemUser = require("../../models/EcosystemUser");
 const Template = require("../../models/Templates");
+const { Op } = require("sequelize");
 
 const popularEcosystems = async (req, res) => {
   try {
@@ -15,29 +17,103 @@ const popularEcosystems = async (req, res) => {
         .json({ message: "User has no created ecosystems" });
     }
 
-    const ecosystemsWithLogos = await Promise.all(topEcosystems.map(async (ecosystem) => {
-      const template = await Template.findOne({ ecosystemId: ecosystem._id });
-      return {
-        ...ecosystem.toObject(),
-        logo: template ? template.navbar.logo : null
-      };
-    }));
+    const ecosystemsWithLogos = await Promise.all(
+      topEcosystems.map(async (ecosystem) => {
+        const template = await Template.findOne({ ecosystemId: ecosystem._id });
+        return {
+          ...ecosystem.toObject(),
+          logo: template ? template.navbar.logo : null,
+        };
+      })
+    );
 
     res.status(200).json({ ecosystemsWithLogos });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error });
+    res.status(500).json({ message: "Internal Server Error", detail: error });
   }
 };
 
-// const allEcosystemUsers = async (req,res)=> {
-//     try {
-//         const creatorId = req.params.creatorId;
+const allEcosystemUsers = async (req, res) => {
+  try {
+    const creatorId = req.params.creatorId;
 
+    const ecosystems = await Ecosystem.find({ creatorId });
+    if (!ecosystems) {
+      return res
+        .status(404)
+        .json({ message: "User has no created Ecosystems" });
+    }
 
-//     } catch (error) {
-        
-//     }
-// }
+    const ecosystemDomains = ecosystems.map(
+      (ecosystem) => ecosystem.ecosystemDomain
+    );
 
-module.exports = { popularEcosystems };
+    if (ecosystemDomains.length === 0) {
+      return res.status(404).json({ message: "No ecosystem domains found" });
+    }
+
+    const ecosystemUsers = await EcosystemUser.findAll({
+      where: {
+        ecosystemDomain: {
+          [Op.in]: ecosystemDomains,
+        },
+      },
+    });
+
+    if (ecosystemUsers.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No users found for the given ecosystems" });
+    }
+
+    res.status(200).json({ ecosystemUsers });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error", detail: error });
+  }
+};
+
+const usersPerEcosystem = async (req, res) => {
+  try {
+    const { creatorId, ecosystemId } = req.body;
+
+    const ecosystems = await Ecosystem.find({ creatorId });
+    if (!ecosystems) {
+      return res
+        .status(404)
+        .json({ message: "User has no created Ecosystems" });
+    }
+
+    const isEcosystemCreator = ecosystems.find(
+      (ecosystem) => ecosystem._id.toString() === ecosystemId.toString()
+    );
+ 
+    if (!isEcosystemCreator) {
+      return res
+        .status(400)
+        .json({ message: "You are not permitted to access this Ecosystem!!" });
+    }
+
+    const domain = isEcosystemCreator.ecosystemDomain;
+
+    const users = await EcosystemUser.findAll({
+      where: {
+        ecosystemDomain: domain,
+      },
+    });
+
+    if (!users.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "There are no users in this ecosystem" });
+    }
+
+    res.status(200).json({ users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error", detail: error });
+  }
+};
+
+module.exports = { popularEcosystems, allEcosystemUsers, usersPerEcosystem };
