815903963742de0afd828add5cd31f3601ee0a04
Ajibade-gfa <Ajibade@gfa-tech.com>
2024-07-17 13:58:46 +0100

    Add servicerequest Endpoints

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use("/api/v1/", require("./src/routes/creatorDashboard"));
 app.use("/api/v1/", require("./src/routes/EcosystemConflictResolution.js"));
 app.use("/api/v1/", require("./src/routes/payment"));
 app.use("/api/v1/", require("./src/routes/setting"));
+app.use("/api/v1/", require("./src/routes/serviceRequest"));
 
 
 app.listen(PORT, () => {
