c2e0d8410b30e88829aad19acd1e041089bafd15
Samuel-Makinde <makindesamuel1999@gmail.com>
2024-08-19 18:24:33 +0100

    made changes to it

--- src/controllers/EcosystemController/createTemplate.js
+++ src/controllers/EcosystemController/createTemplate.js
@@ -110,7 +110,7 @@ const getAnEcosystemTemplate = async (req, res) => {
       return res.status(404).json({ message: "Ecosystem not found" });
     }
 
-    const template = await Template.findOne({ _id: ecosystem.templates });
+    const template = await CreatorTemplate.findOne({ _id: ecosystem.templates });
     console.log("this is template", template);
 
     if (!template) {
