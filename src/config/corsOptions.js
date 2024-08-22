b934952d8c2b48731137ad6e51428851cacb4fad
Samuel-Makinde <makindesamuel1999@gmail.com>
2024-08-22 12:39:08 +0100

    made new changes to cors

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -7,7 +7,8 @@ const whiteList = [
   "https://dimpified-frontend-testing.azurewebsites.net"
 ];
 
-const allowedDomainsRegex = /^https?:\/\/([a-zA-Z0-9-]+\.)*dimpified\.com(:\d{1,5})?$/;
+
+const allowedDomainsRegex = /^https?:\/\/([a-zA-Z0-9-]+\.)?(localhost|dimpified\.com)(:\d{1,5})?$/;
 
 const corsOptions = {
   origin: (origin, callback) => {
@@ -26,3 +27,34 @@ module.exports = corsOptions;
 
 
 
+// const whiteList = [
+//   "http://localhost:5175",
+//   "http://localhost:5173",
+//   "http://localhost:5174",
+//   "https://www.dimpified.com",
+//   "https://dimpified.com",
+//   "https://dimpified-frontend-testing.azurewebsites.net"
+// ];
+
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     // Check if the origin is in the whitelist
+//     if (whiteList.indexOf(origin) !== -1 || !origin) {
+//       return callback(null, true);
+//     }
+    
+//     // Allow all subdomains of dimpified.com
+//     const dimpifiedRegex = /^https:\/\/([a-zA-Z0-9-]+\.)?dimpified\.com$/;
+//     if (dimpifiedRegex.test(origin)) {
+//       return callback(null, true);
+//     }
+
+//     // Deny access if the origin doesn't match any of the allowed patterns
+//     return callback(new Error("Access denied by CORS"));
+//   },
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   optionsSuccessStatus: 200,
+//   credentials: true,
+// };
+
+// module.exports = corsOptions;
