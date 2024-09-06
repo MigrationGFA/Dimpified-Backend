b1fcf06b2db1b602c7af42cb0b64fb42cc9fab52
Ajibade-gfa <Ajibade@gfa-tech.com>
2024-09-06 13:32:02 +0100

    Add onsite booking endpoint

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -4,11 +4,13 @@ const {
   getBookings,
   bookingOverview,
   changeBookingStatusToCompleted,
+  onsiteBooking,
 } = require("../controllers/serviceRequest/booking");
 
 const router = express.Router();
 
 router.post("/create-booking", createBooking);
+router.post("/onsite-booking", onsiteBooking);
 router.get("/bookings/:ecosystemDomain", getBookings);
 router.get("/booking-overview/:ecosystemDomain", bookingOverview);
 router.put("/complete-booking", changeBookingStatusToCompleted);
