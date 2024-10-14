2c2d2e03e17a41dec2a0e3dc1a628ed74dbbf4f4
Company <adebowale@getfundedafrica.com>
2024-10-14 09:56:01 +0100

    working

--- src/routes/newCreator.js
+++ src/routes/newCreator.js
@@ -8,6 +8,10 @@ const {
 const {
   creatorLogin,
 } = require("../controllers/newAddition/authentication/login");
+const {
+  weeklyBookingStats,
+  weeklyIncomeStats,
+} = require("../controllers/newAddition/PaymentController/Statistics");
 const router = express.Router();
 
 router.post("/creator/signup", creatorSignup);
@@ -15,5 +19,9 @@ router.post("/creator/sign-in", creatorLogin);
 router.post("/creator/verify-otp", verifyOTPCreator);
 router.post("/creator/business-details", createBusinessDetails);
 router.get("/all-creators", getCreators);
+
+// Statistics
+router.get("/booking-stats/:ecosystemDomain", weeklyBookingStats);
+router.get("/income-stats/:ecosystemDomain", weeklyIncomeStats);
 
 module.exports = router;
