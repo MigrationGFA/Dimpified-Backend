7d52c275dc998aebc2931a015b83a7f873d1d094
Samuel-Makinde19 <158587519+Samuel-Makinde19@users.noreply.github.com>
2024-11-13 14:05:16 +0100

    Delete src directory

--- src/routes/newPayment.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {
-  verifySubscription,
-} = require("../controllers/newAddition/PaymentController/Subscription");
-
-router.post("/payment/verify-subscription", verifySubscription);
-module.exports = router;
