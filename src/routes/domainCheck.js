c405954a9f8aafb9921a1f0473a7e4d8cd48cd32
Samuel-Makinde19 <158587519+Samuel-Makinde19@users.noreply.github.com>
2024-11-13 13:58:14 +0100

    Delete src directory

--- src/routes/domainCheck.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const checkDomainAvailability = require("../controllers/EcosystemController/checkDomain");
-
-// Endpoint to check domain availability
-router.post("/check-domain", checkDomainAvailability);
-
-module.exports = router;
