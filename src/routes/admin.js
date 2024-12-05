442a758282355ccb8569b54eb8066ef5365e4d33
Company <adebowale@getfundedafrica.com>
2024-12-05 14:14:16 +0000

    working

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const adminController = require("../controllers/AdminController/dashboard");
 const adminSubscriptionController = require("../controllers/AdminController/subscription");
 const adminTransactionController = require("../controllers/AdminController/transaction");
-const authenticatedUser = require("../middleware/authentication");
+// const authenticatedUser = require("../middleware/authentication");
 
 const {
   monthlySubscriptions,
@@ -20,65 +20,65 @@ const {
 
 router.get(
   "/admin/all-users",
-  authenticatedUser,
+
   adminController.getAdminDashboardUsers
 );
 router.get(
   "/admin/all-subscribers",
-  authenticatedUser,
+
   adminController.getAdminDashboardSubscribers
 );
 router.get(
   "/admin/all-users-informations",
-  authenticatedUser,
+
   adminController.getAdminDashboardUsersInformations
 );
 
 router.get(
   "/admin/all-subscription",
-  authenticatedUser,
+
   adminSubscriptionController.getAllSubscriptions
 );
 
 router.get(
   "/admin/total-subscriptions",
-  authenticatedUser,
+
   adminSubscriptionController.getTotalSubscriptions
 );
 
 router.get(
   "/admin/subscriptions",
-  authenticatedUser,
+
   adminSubscriptionController.getCalculatedTotalSubscriptions
 );
 
 router.get(
   "/admin/all-creator-withdrawals",
-  authenticatedUser,
+
   adminTransactionController.getAllCreatorEarnings
 );
 
-router.get("/ecosystem-transactions", authenticatedUser, ecosystemTransactions);
+router.get("/ecosystem-transactions", ecosystemTransactions);
 
-router.get("/ecosystem-user-stats", authenticatedUser, userStats);
+router.get("/ecosystem-user-stats", userStats);
 
 router.get(
   "/ecosystem-monthly-registration",
-  authenticatedUser,
+
   monthlyRegistration
 );
 
-router.get("/ecosystem-supports-tickets", authenticatedUser, supportTickets);
+router.get("/ecosystem-supports-tickets", supportTickets);
 
 router.get(
   "/ecosystem-subscription-plan-type-and-total-subscription",
-  authenticatedUser,
+
   planTypeTotalSubscription
 );
 
 router.get(
   "/ecosystem-monthly-subscription",
-  authenticatedUser,
+
   monthlySubscriptions
 );
 
