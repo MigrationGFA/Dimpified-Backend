3c6d04df0f12beeec9ae5117e71d6635a5bc9417
Ajibade-gfa <Ajibade@gfa-tech.com>
2025-03-10 11:50:26 +0100

    fix complete bookinh

--- src/routes/support.js
+++ src/routes/support.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const supportController = require("../controllers/Support/support");
+const authenticatedUser = require("../middleware/authentication");
+
+
 
 // Create a new support ticket
 router.post("/create/support-ticket", supportController.createSupportTicket);
 
 // Get all support tickets for a merchant (filtered by status)
 router.get(
-  "/support-tickets/:ecosystemDomain",
+  "/support-tickets/:ecosystemDomain",authenticatedUser,
   supportController.getMerchantSupportTickets
 );
 
 // Get a single support ticket by ID
-router.get("/support-ticket/:id", supportController.getSupportTicketById);
+router.get("/support-ticket/:id",authenticatedUser, supportController.getSupportTicketById);
 
 router.get(
-  "/support-box/:ecosystemDomain",
+  "/support-box/:ecosystemDomain",authenticatedUser,
   supportController.getSupportBoxStats
 );
 
 //reply ticket
-router.post("/reply-ticket", supportController.replyToSupportTicket);
+router.post("/reply-ticket",authenticatedUser, supportController.replyToSupportTicket);
 
 module.exports = router;
